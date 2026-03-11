1dd83d6c4f0462ec27c5ca38073e3ff9f224eea8
kantho202 <kanthochakma2025@gmail.com>
2026-03-11 13:43:22 +0600

    payment success page update

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -3,27 +3,29 @@ import { Link, useSearchParams } from 'react-router';
 import useAxiosSecure from '../../../hook/useAxiosecure';
 
 const PaymentSuccess = () => {
-    const  [searchParams]=useSearchParams()
-    const axiosSecure =useAxiosSecure()
-    const [paymentInfo ,setPaymentInfo]=useState({})
-    const sessionId =searchParams.get('session_id')
-    
+    const [searchParams] = useSearchParams()
+    const axiosSecure = useAxiosSecure()
+    const [paymentInfo, setPaymentInfo] = useState({})
+    const sessionId = searchParams.get('session_id')
 
-    useEffect(()=>{
-        if(sessionId){
+
+    useEffect(() => {
+        if (sessionId) {
             axiosSecure.patch(`/payment-success?session_id=${sessionId}`)
-            .then(res=>{
-               console.log(res.data)
-                setPaymentInfo({transactionId: res.data.transactionId})
-            })
+                .then(res => {
+                    console.log(res.data)
+                    setPaymentInfo({ transactionId: res.data.transactionId })
+                })
         }
-    },[sessionId,axiosSecure])
+    }, [sessionId, axiosSecure])
     return (
         <div className='flex flex-col justify-center px-3 space-y-3 items-center min-h-screen'>
             <h1 className="text-2xl lg:text-3xl font-bold text-primary">Payment Successfully</h1>
             <h1 className="text-[18px] lg:text-xl font-bold text-center text-primary">TransactionId : {paymentInfo?.transactionId}</h1>
-            <Link to="/dasboard"><button className="btn btn-primary">Back</button></Link>
-            <Link to="/"><button className="btn btn-primary">Home</button></Link>
+            <div className='flex gap-5'>
+                <Link to="/dasboard/myBookedTickets"><button className="btn btn-primary">Back</button></Link>
+                <Link to="/"><button className="btn btn-primary">Home</button></Link>
+            </div>
             {/* <h2>Trancaction id:{paymentInfo?.transactionId}</h2> */}
         </div>
     );
